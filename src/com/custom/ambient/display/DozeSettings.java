NusantaraProject-ROM/android_packages_apps_CustomDoze

CustomDoze: Use SwitchPreference from VendorSupport

src/com/custom/ambient/display/DozeSettings.java
@@ -22,11 +22,12 @@ import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.provider.Settings;
-import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
-import androidx.preference.PreferenceFragment;
 import android.view.MenuItem;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+
+import com.havoc.support.preferences.SwitchPreference;
 import com.havoc.support.preferences.SystemSettingSeekBarPreference;
 import com.havoc.support.preferences.SystemSettingSwitchPreference;
 

Octavi-OS/platform_packages_apps_CustomDoze

Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

src/com/custom/ambient/display/DozeSettings.java
@@ -23,10 +23,14 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.provider.Settings;
 import androidx.preference.PreferenceCategory;
-import androidx.preference.Preference;
-import androidx.preference.SwitchPreference;
-import androidx.preference.PreferenceFragment;
 import android.view.MenuItem;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+
+import com.octavi.support.preferences.SwitchPreference;
+import com.octavi.support.preferences.SystemSettingSeekBarPreference;
+import com.octavi.support.preferences.SystemSettingSwitchPreference;
 
 public class DozeSettings extends PreferenceActivity implements PreferenceFragment.OnPreferenceStartFragmentCallback {
 